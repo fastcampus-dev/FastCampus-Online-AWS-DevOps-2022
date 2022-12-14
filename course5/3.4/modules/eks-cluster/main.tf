725f04ece696e5c87e3ed3574f263dd93eabd11a
yijeong <yijeong.cho@gmail.com>
2022-12-14 18:52:44 +0900

    add karpenter config

--- course5/3.4/modules/eks-cluster/main.tf
+++ course5/3.4/modules/eks-cluster/main.tf
@@ -35,6 +35,10 @@ module "eks" {
   # Cluster Logging Setting
   cluster_enabled_log_types = ["api"]
 
+  tags = {
+      "karpenter.sh/discovery" = local.cluster_name
+    }
+
   # Extend node-to-node security group rules
   node_security_group_additional_rules = {
     ingress_self_all = {
@@ -148,6 +152,13 @@ resource "aws_ec2_tag" "private_subnet_cluster_tag" {
   value       = "owned"
 }
 
+resource "aws_ec2_tag" "private_subnet_karpenter_tag" {
+  for_each    = toset(local.private_subnets)
+  resource_id = each.value
+  key         = "karpenter.sh/discovery/${local.cluster_name}"
+  value       = local.cluster_name
+}
+
 // 퍼블릭 서브넷 태그
 resource "aws_ec2_tag" "public_subnet_tag" {
 for_each    = toset(local.public_subnets)
@@ -161,4 +172,11 @@ for_each    = toset(local.public_subnets)
 resource_id = each.value
 key         = "kubernetes.io/cluster/${local.cluster_name}"
 value       = "owned"
+}
+
+resource "aws_ec2_tag" "public_subnet_karpenter_tag_2" {
+for_each    = toset(local.public_subnets)
+resource_id = each.value
+key         = "karpenter.sh/discovery/${local.cluster_name}"
+value       = local.cluster_name
 }
