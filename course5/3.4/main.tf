37818fa90903e4431644299f3fa652df5985b9fa
nessa.829 <nessa.829@kakaostyle.com>
2023-01-01 21:49:36 +0900

    initial cyj

--- course5/3.4/main.tf
+++ course5/3.4/main.tf
@@ -2,7 +2,7 @@ module "eks" {
   # eks 모듈에서 사용할 변수 정의
   source = "./modules/eks-cluster"
   cluster_name = "fast-cluster"
-  cluster_version = "1.23"
+  cluster_version = "1.24"
   vpc_id = "<VPCID>"
 
   private_subnets = ["<private-subnetID-1>", "<private-subnetID-2>"]
