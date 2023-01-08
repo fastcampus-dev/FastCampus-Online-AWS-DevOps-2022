f57553b6bd5d5b6ec7f09e4a01091615192f0d40
nessa.829 <nessa.829@kakaostyle.com>
2023-01-08 15:44:59 +0900

    initial cyj

---
+++ course5/7.2/cicd.tf
@@ -0,0 +1,46 @@
+locals {
+  tags = {
+    Environment = "test"
+    Terraform   = "true"
+  }
+}
+
+resource "aws_iam_user" "github-action" {
+  name = "github-action"
+  tags = local.tags
+}
+
+resource "aws_iam_user_policy" "github-action-pol" {
+  name = "github-action-pol"
+  user = aws_iam_user.github-action.name
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "AllowPush",
+            "Effect": "Allow",
+            "Action": [
+                "ecr:GetDownloadUrlForLayer",
+                "ecr:BatchGetImage",
+                "ecr:BatchCheckLayerAvailability",
+                "ecr:PutImage",
+                "ecr:InitiateLayerUpload",
+                "ecr:UploadLayerPart",
+                "ecr:CompleteLayerUpload"
+            ],
+            "Resource": "arn:aws:ecr:ap-northeast-2:${data.aws_caller_identity.current.account_id}:repository/fast-app"
+        },
+        {
+            "Sid": "GetAuthorizationToken",
+            "Effect": "Allow",
+            "Action": [
+                "ecr:GetAuthorizationToken"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
